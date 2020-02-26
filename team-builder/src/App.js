e4b478342852487d17dd6102461000c04e48394e
fbzr <fabricio.spb@gmail.com>
2020-02-26 09:51:55 -0800

    create state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Form from './components/Form';
 
 function App() {
+  const [members, setMembers] = useState([]);
+
+  const addMember = ({ name, email, role }) => {
+    setMembers([...members, {
+      name,
+      email,
+      role
+    }]);
+  }
+
   return (
     <Fragment>
       <h1>App</h1>
-      <Form />
+      <Form addMember={addMember} />
     </Fragment>
   );
 }
