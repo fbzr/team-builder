8af716395989362c52f9701cc729ac82bfdfef71
fbzr <fabricio.spb@gmail.com>
2020-02-26 11:08:26 -0800

    display members info

--- team-builder/src/components/MembersList.js
+++ team-builder/src/components/MembersList.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+const MemberInfo = ({ member }) => {
+    const { name, email, role } = member;
+    return (
+        <div>
+            <p>Name: {name}</p>
+            <p>Email: {email}</p>
+            <p>Role: {role}</p>
+        </div>
+    );
+}
+
 const MembersList = ({ members }) => {
     return (
         <div>
-            
+            <h1>Members List</h1>
+            { members.map((member, i) => <MemberInfo member={member} key={i} />) }
         </div>
     )
 }
+
+
 
 export default MembersList
