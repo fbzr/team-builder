1ec92a7ddc7af0c493a0da20ff03d091131eddfb
fbzr <fabricio.spb@gmail.com>
2020-02-26 12:30:42 -0800

    commit before implementing editing

--- team-builder/src/components/MemberInfo.js
+++ team-builder/src/components/MemberInfo.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const MemberInfo = ({ member }) => {
     const { name, email, role } = member;
+
+    const editInfo = e => {
+        e.preventDefault();
+        console.log('edit clicked');
+    }
     return (
         <div>
             <p>Name: {name}</p>
             <p>Email: {email}</p>
             <p>Role: {role}</p>
+            <button onClick={editInfo}>Edit info</button>
         </div>
     );
 }
--- team-builder/src/components/MembersList.js
+++ team-builder/src/components/MembersList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import MemberInfo from './MemberInfo';
 
-const MembersList = ({ members }) => {
+const MembersList = ({ members, editMember }) => {
     return (
         <div>
             {members.length > 0 ? <h1>Members List</h1> : null}
-            { members.map((member, i) => <MemberInfo member={member} key={i} />) }
+            { members.map((member, i) => <MemberInfo member={member} key={i} editMember={editMember} />) }
         </div>
     )
 }
